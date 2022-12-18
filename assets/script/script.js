8540593f312fe03ab20f96107c50bb6b255a121e
Jacob-Jeffries <jeffrij85@gmail.com>
2022-12-18 15:21:28 -0500

    Completed the function that loads a new question and buttons

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,6 +4,7 @@ let timeRemaining = document.querySelector('#time');
 let testArea = document.querySelector('#test-wrapper');
 let q = document.querySelector('#Q');
 let prompt = document.querySelector('#prompt');
+let btns = document.querySelector('#btns');
 let start = document.querySelector('#start');
 
 let highScore = localStorage.getItem('highScore');
@@ -26,12 +27,50 @@ let questions = [
   }
 ];
 
-console.log(questions[0]Q[0]);
+console.log(questions[0]["Q"]);
+
+function newQuestion (){
+  q.innerText = questions[0]["Q"];
+    prompt.style.display = "none";
+    start.style.display = "none";
+
+    let btnA = document.createElement("button");
+      btnA.setAttribute("type", "button");
+      btnA.setAttribute("id", "A");
+      btnA.setAttribute("class", "btn bg-dark rounded btn-outline-secondary text-white w-100");
+      btnA.innerText = questions[0]["A"][1];
+      btns.appendChild(btnA);
+
+    let btnB = document.createElement("button");
+      btnB.setAttribute("type", "button");
+      btnB.setAttribute("id", "B");
+      btnB.setAttribute("class", "btn bg-dark rounded btn-outline-secondary text-white w-100");
+      btnB.innerText = questions[0]["B"][1];
+      btns.appendChild(btnB);
+
+    let btnC = document.createElement("button");
+      btnC.setAttribute("type", "button");
+      btnC.setAttribute("id", "C");
+      btnC.setAttribute("class", "btn bg-dark rounded btn-outline-secondary text-white w-100");
+      btnC.innerText = questions[0]["C"][1];
+      btns.append(btnC);
+
+    let btnD = document.createElement("button");
+      btnD.setAttribute("type", "button");
+      btnD.setAttribute("id", "D");
+      btnD.setAttribute("class", "btn bg-dark rounded btn-outline-secondary text-white w-100");
+      btnD.innerText = questions[0]["D"][1];
+      btns.append(btnD);
+};
+
+function selectAnswer(){
+  
+};
+
 
 function main(){
-  for(let i=0; i < questions.length; i++){
-    q.innerText = questions[0][0];
-  }
+  newQuestion();
+
 };
 
 start.addEventListener("click", main);
